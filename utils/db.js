8c095f2e7e034c3b1ea86400249188a77e5080c2
Makarona10 <ahmedneyssi@gmail.com>
2024-09-27 04:52:58 +0300

    Task 1 done

---
+++ utils/db.js
@@ -0,0 +1,26 @@
+import MongoClient from 'mongodb/lib/mongo_client';
+
+class DBClient {
+  constructor() {
+    const HOST = process.env.DB_HOST || 'localhost';
+    const PORT = process.env.DB_PORT || '27017';
+    const DB = process.env.DB_DATABASE || 'files_manager';
+    const URL = `mongodb://${HOST}:${PORT}/${DB}`;
+    this.client = new MongoClient(URL);
+    this.client.connect();
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    return this.client.db().collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
+}
+
+module.exports = DBClient;
