6619761f39540705a3578e8cc252c5ee9af1b782
Makarona10 <ahmedneyssi@gmail.com>
2024-09-28 18:06:28 +0300

    Task 2 done

---
+++ controllers/AppController.js
@@ -0,0 +1,16 @@
+import DBClient from '../utils/db.js';
+import RedisClient from '../utils/redis.js';
+
+export default class AppController {
+  static getStatus(_, res) {
+    const redisStat = RedisClient.isAlive();
+    const dbStat = DBClient.isAlive();
+    return res.status(200).json({ redis: redisStat, db: dbStat });
+  }
+
+  static async getStats(_, res) {
+    const files = await DBClient.nbFiles();
+    const users = await DBClient.nbUsers();
+    return res.status(200).json({ users, files });
+  }
+}
