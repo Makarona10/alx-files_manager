444cf0a80bb5a2889a91922d53c6637b33c829e2
Makarona10 <ahmedneyssi@gmail.com>
2024-09-29 23:50:41 +0300

    Task 4 done

---
+++ middleware/auth.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+import { getUserFromXToken, getUserFromAuthorization } from '../utils/auth';
+
+export const basicAuthenticate = async (req, res, next) => {
+  const user = await getUserFromAuthorization(req);
+
+  if (!user) {
+    res.status(401).json({ error: 'Unauthorized' });
+    return;
+  }
+  req.user = user;
+  next();
+};
+
+export const xTokenAuthenticate = async (req, res, next) => {
+  const user = await getUserFromXToken(req);
+
+  if (!user) {
+    res.status(401).json({ error: 'Unauthorized' });
+    return;
+  }
+  req.user = user;
+  next();
+};
--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,15 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import { basicAuthenticate, xTokenAuthenticate } from '../middleware/auth';
 
 const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.post('users', UsersController.postNew);
-router.get('connect', AuthController.getConnect);
-router.post('disconnect', AuthController.getDisconnect);
-router.post('users/me', UsersController.getMe);
+router.post('/users', UsersController.postNew);
+router.get('/connect', basicAuthenticate, AuthController.getConnect);
+router.post('/disconnect', xTokenAuthenticate, AuthController.getDisconnect);
+router.post('/users/me', xTokenAuthenticate, UsersController.getMe);
 
 export default router;
