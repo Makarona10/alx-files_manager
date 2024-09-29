d6534801562fb4c80c5114343eadbcbef856edcc
Makarona10 <ahmedneyssi@gmail.com>
2024-09-29 23:40:44 +0300

    Task 4 done

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,9 @@ const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.post('/users', UsersController.postNew);
-router.get('/connect ', AuthController.getConnect);
-router.post('/disconnect', AuthController.getDisconnect);
-router.post('/users/me', UsersController.getMe);
+router.post('users', UsersController.postNew);
+router.get('connect', AuthController.getConnect);
+router.post('disconnect', AuthController.getDisconnect);
+router.post('users/me', UsersController.getMe);
 
 export default router;
