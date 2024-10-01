8552bdea7304e2df750092c9b48a577f4743f88b
Makarona10 <ahmedneyssi@gmail.com>
2024-10-02 00:50:24 +0300

    Task 5 done

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import { config } from 'dotenv';
 import router from './routes/index';
 
 const app = express();
+config();
 
 const PORT = process.env.PORT || 5000;
 
